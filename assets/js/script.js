92aed97ddb799abe63467df9545cd22eb862510c
Macy Kay Mannix <macymannix@gmail.com>
2022-04-19 10:23:41 -0700

    add tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
 var buttonEL = document.querySelector("#save-task");
 console.log(buttonEL);
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// buttonEl.addEventListener("click", function() {
+//   var listItemEl = document.createElement("li");
+//   listItemEl.className = "task-item";
+//   listItemEl.textContent = "This is a new task.";
+//   tasksToDoEl.appendChild(listItemEl);
+// });
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "this is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEL.addEventListener("click", createTaskHandler);
+
